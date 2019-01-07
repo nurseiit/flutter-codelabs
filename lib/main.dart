28c500f77661a3d7859760bdbdb63d73869eca74
Nurseiit Abdimomyn <nurseyit.abdimomyn@gmail.com>
2019-01-07 18:11:08 +0900

    Refactor initial code

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  new MaterialApp(
-    title: "Friendlychat",
-    home: new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Friendlychat"),
-      ),
-    ),
-  ),
-);
+void main() => runApp(new FriendlychatApp());
+
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Friendlychat",
+      home: new ChatScreen()
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Friendlychat"))
+    );
+  }
+}
