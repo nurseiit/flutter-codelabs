2f99978c09d597cb3bf884ca8b10b74ec2b89f91
Nurseiit Abdimomyn <nurseyit.abdimomyn@gmail.com>
2019-01-08 12:44:14 +0900

    Install autoformat && add trailing commas

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class FriendlychatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "Friendlychat",
-      home: new ChatScreen()
+      home: new ChatScreen(),
     );
   }
 }
@@ -26,7 +26,7 @@ class ChatScreenState extends State<ChatScreen> {
   void _handleSubmitted(String text) {
     _textController.clear();
     ChatMessage message = new ChatMessage(
-      text: text
+      text: text,
     );
     setState(() {
       _messages.insert(0, message);
@@ -44,42 +44,47 @@ class ChatScreenState extends State<ChatScreen> {
               padding: new EdgeInsets.all(8.0),
               reverse: true,
               itemBuilder: (_, int index) => _messages[index],
-              itemCount: _messages.length
-            )
+              itemCount: _messages.length,
+            ),
           ),
           new Divider(height: 1.0),
           new Container(
-            decoration: new BoxDecoration(color: Theme.of(context).cardColor),
-            child: _buildTextComposer()
-          )
-        ]
-      )
+            decoration: new BoxDecoration(
+              color: Theme.of(context).cardColor,
+            ),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
     );
   }
+
   Widget _buildTextComposer() {
     return new IconTheme(
-      data: new IconThemeData(color: Theme.of(context).accentColor),
+      data: new IconThemeData(
+        color: Theme.of(context).accentColor,
+      ),
       child: new Container(
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0,),
         child: new Row(children: <Widget>[
           new Flexible(
             child: new TextField(
               controller: _textController,
               onSubmitted: _handleSubmitted,
               decoration: new InputDecoration.collapsed(
-                hintText: "Send a message"
+                hintText: "Send a message",
               ),
-            )
+            ),
           ),
           new Container(
             margin: new EdgeInsets.symmetric(horizontal: 4.0),
             child: new IconButton(
               icon: new Icon(Icons.send),
-              onPressed: () => _handleSubmitted(_textController.text)
-            )
-          )
-        ])
-      )
+              onPressed: () => _handleSubmitted(_textController.text),
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
@@ -96,7 +101,7 @@ class ChatMessage extends StatelessWidget {
         children: <Widget>[
           new Container(
             margin: const EdgeInsets.only(right: 16.0),
-            child: new CircleAvatar(child: new Text(_name[0]))
+            child: new CircleAvatar(child: new Text(_name[0])),
           ),
           new Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -104,12 +109,12 @@ class ChatMessage extends StatelessWidget {
               new Text(_name, style: Theme.of(context).textTheme.subhead),
               new Container(
                 margin: const EdgeInsets.only(top: 5.0),
-                child: new Text(text)
-              )
-            ]
-          )
-        ]
-      )
+                child: new Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
