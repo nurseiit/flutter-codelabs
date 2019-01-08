80f8901b79b250a57990304830ee504ac6cc9086
Nurseiit Abdimomyn <nurseyit.abdimomyn@gmail.com>
2019-01-08 12:02:05 +0900

    Create ChatMessage widget & clear trailing commas

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new FriendlychatApp());
 
+const String _name = "Nurka";
+
 class FriendlychatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -50,10 +52,40 @@ class ChatScreenState extends State<ChatScreen> {
             margin: new EdgeInsets.symmetric(horizontal: 4.0),
             child: new IconButton(
               icon: new Icon(Icons.send),
-              onPressed: () => _handleSubmitted(_textController.text),
+              onPressed: () => _handleSubmitted(_textController.text)
             )
           )
-        ],)
+        ])
+      )
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: new CircleAvatar(child: new Text(_name[0]))
+          ),
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(_name, style: Theme.of(context).textTheme.subhead),
+              new Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: new Text(text)
+              )
+            ]
+          )
+        ]
       )
     );
   }
