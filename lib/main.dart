6d2de87b373bbd33133c12c69ccf998914b7f3ae
Nurseiit Abdimomyn <nurseyit.abdimomyn@gmail.com>
2019-01-08 11:06:06 +0900

    Create a text field with send button

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,46 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("Friendlychat"))
+      appBar: new AppBar(title: new Text("Friendlychat")),
+      body: _buildTextComposer()
+    );
+  }
+  Widget _buildTextComposer() {
+    return new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new Row(children: <Widget>[
+        new Flexible(
+          child: new TextField(
+            controller: _textController,
+            onSubmitted: _handleSubmitted,
+            decoration: new InputDecoration.collapsed(
+              hintText: "Send a message"
+            ),
+          )
+        ),
+        new Container(
+          margin: new EdgeInsets.symmetric(horizontal: 4.0),
+          child: new IconButton(
+            icon: new Icon(Icons.send),
+            onPressed: () => _handleSubmitted(_textController.text),
+          )
+        )
+      ],)
     );
   }
 }
