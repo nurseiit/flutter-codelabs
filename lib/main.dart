221996e9adbe957c2beadbfaf87495f7f4812f80
Nurseiit Abdimomyn <nurseyit.abdimomyn@gmail.com>
2019-01-08 12:15:08 +0900

    Create messages send & display widgets

--- lib/main.dart
+++ lib/main.dart
@@ -20,17 +20,40 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("Friendlychat")),
-      body: _buildTextComposer()
+      body: new Column(
+        children: <Widget>[
+          new Flexible(
+            child: new ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length
+            )
+          ),
+          new Divider(height: 1.0),
+          new Container(
+            decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextComposer()
+          )
+        ]
+      )
     );
   }
   Widget _buildTextComposer() {
