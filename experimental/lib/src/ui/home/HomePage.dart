f4f923d082bf586e3169ce1b63b3a7f0886fc1cd
Nurseiit Abdimomyn <nurseyit.abdimomyn@gmail.com>
2019-01-09 17:03:43 +0900

    Create simple AppBar with text

--- experimental/lib/src/ui/home/HomePage.dart
+++ experimental/lib/src/ui/home/HomePage.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'package:experimental/src/ui/home/GradientAppBar.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new Text("Hello"),
+      body: new Column(
+        children: <Widget>[
+          new GradientAppBar("Frooto"),
+        ],
+      ),
     );
   }
 }
