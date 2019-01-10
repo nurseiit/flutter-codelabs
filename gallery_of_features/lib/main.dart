490db70f4ee8ea503074c249ddd3cfda1eb2ae87
Nurseiit Abdimomyn <nurseyit.abdimomyn@gmail.com>
2019-01-10 13:24:27 +0900

    Combine login page with Flutter Gallery app

--- gallery_of_features/lib/main.dart
+++ gallery_of_features/lib/main.dart
@@ -1,14 +1,22 @@
-// Copyright 2015 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Thanks for checking out Flutter!
-// Like what you see? Tweet us @flutterio
-
 import 'package:flutter/material.dart';
+import 'package:flutter_gallery/ui/login_page.dart';
 
 import 'gallery/app.dart';
 
-void main() {
-  runApp(const GalleryApp());
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Frooto',
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => new LoginPage(),
+          '/home': (context) => const GalleryApp(),
+        });
+  }
 }
